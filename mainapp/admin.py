1a8c26b74dd1554d82636267384ece33b87d93bc
ciprianoe <ciprianoescorche@gmail.com>
2023-08-05 20:06:05 -0400

    config models task

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
+
+#para ver el campo que no se vizualiza en el panel 
+class TaskAdmin(admin.ModelAdmin):
+    readonly_fields = ("created",)
+
 
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+""" modelo para las task """
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)   #este campo no se vizualiza en el panel admi
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title +' By: '+ self.user.username
